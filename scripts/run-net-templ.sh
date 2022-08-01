88bf6790dafa3438791b6dfd63834862fa6dd05e
Dror Tirosh <dror@opengsn.org>
2022-08-01 18:27:35 +0300

    jq

--- scripts/run-net-templ.sh
+++ scripts/run-net-templ.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -e
-jq '.[][]' networks/gsn-networks.json | jq -s > /tmp/tmp.json
+#!/bin/bash -xe
+jq '.[][]' networks/gsn-networks.json > /tmp/tmp1
+cat /tmp/tmp1 | jq -s > /tmp/tmp.json
 mustache /tmp/tmp.json `dirname $0`/net.tmpl > /tmp/tmp.sh
 (cd networks; source /tmp/tmp.sh)
 
